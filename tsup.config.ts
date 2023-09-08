c4e800fa44040117c04590caffeda6581079f6a9
Jon Ursenbach <jon@ursenba.ch>
2023-09-08 10:44:19 -0700

    fix: linting problem

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,7 @@
-import type { Options } from 'tsup';
-
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from 'tsup';
 
-export default defineConfig((options) => ({
+export default defineConfig(options => ({
   ...options,
 
   cjsInterop: true,
